69567c6ea3545f4b5ae036cba649279c441ccd7c
nemesisesq <nemesisesq@gmail.com>
2018-06-18 23:54:02 -0400

    y

--- src/opportunities/opportunities.main.js
+++ src/opportunities/opportunities.main.js
@@ -6,6 +6,7 @@ import Grid from "@material-ui/core/Grid";
 import Button from "@material-ui/core/es/Button/Button";
 import AddIcon from "@material-ui/icons/Add"
 import SaveIcon from "@material-ui/icons/Save"
+import EditIcon from "@material-ui/icons/Edit"
 import TextField from "@material-ui/core/es/TextField/TextField";
 import {withStyles} from '@material-ui/core/styles';
 import _ from 'lodash'
@@ -13,11 +14,13 @@ import _ from 'lodash'
 const hostUri = "http://localhost:3000/api";
 
 const OpportunityList = props => {
+    const {select, opportunities} = props
+
     return (
         <List>
-            {props.opportunities.map(item => {
+            {opportunities.map(item => {
                 return (
-                    <ListItem key={item.id}>
+                    <ListItem button onClick={_ => select(item)} key={item.id}>
                         {item.name}
                         |
                         {item.summary}
@@ -35,19 +38,19 @@ const OpportunityDetail = props => {
             {Object.keys(detail)
                 .filter(x => _.includes(['name', 'summary', 'business_category'], x))
                 .map(key => {
-                return (
-                    <ListItem key={key}>
-                        <TextField
-                            id={key}
-                            label={key}
-                            value={detail[key]}
-                            onChange={data => props.handleChange(key, data)}
-                            margin="normal"
-                            disabled={!props.editing}
-                        />
-                    </ListItem>
-                )
-            })}
+                    return (
+                        <ListItem key={key}>
+                            <TextField
+                                id={key}
+                                label={key}
+                                value={detail[key]}
+                                onChange={data => props.handleChange(key, data)}
+                                margin="normal"
+                                disabled={!props.editing}
+                            />
+                        </ListItem>
+                    )
+                })}
         </List>
     )
 }
@@ -60,7 +63,8 @@ class Opportunities extends Component {
         this.state = {
             opportunities: [{name: 'hello world', id: 1}, {name: "wornderful", id: 2}],
             detail: {},
-            editing: true
+            editing: false,
+            updating: false
         }
     }
 
@@ -86,7 +90,8 @@ class Opportunities extends Component {
 
     _newOpp = () => {
         this.setState({
-            editing: true
+            editing: true,
+            updating: false
         })
         axios({
             method: "get",
@@ -105,22 +110,48 @@ class Opportunities extends Component {
 
     _saveDetail = () => {
 // better handling is needed
-        axios({
-            method: "post",
-            url: `${hostUri}/opportunities`,
-            responseType: "application/json",
-            data: this.state.detail
-        })
-            .then(data => {
-                debugger
-                this.setState({
-                    editing : false
-                })
-            })
-            .catch(error => {
-                console.log(error)
-            })
+        if (!this.state.editing) {
+            this.setState({
+                editing: true
+            });
+            return
+        }
 
+        if (this.state.updating) {
+            axios({
+                method: "put",
+                url: `${hostUri}/opportunities/${this.state.detail.id}`,
+                responseType: "application/json",
+                data: this.state.detail
+            })
+                .then(data => {
+                    debugger
+                    this.setState({
+                        editing: false
+                    })
+                })
+                .catch(error => {
+                    console.log(error)
+                })
+        } else {
+
+            axios({
+                method: "post",
+                url: `${hostUri}/opportunities`,
+                responseType: "application/json",
+                data: this.state.detail
+            })
+                .then(data => {
+                    debugger
+                    this.setState({
+                        editing: false
+                    })
+                })
+                .catch(error => {
+                    console.log(error)
+                })
+
+        }
     }
     _handleChange = (key, data) => {
 
@@ -133,11 +164,23 @@ class Opportunities extends Component {
         console.log(data)
     }
 
+    _selectDetail = item => {
+        this.setState({
+            detail: item,
+            updating: true
+        })
+    }
+
+    _cancelEdit = _ => {
+        this.setState({
+            editing: false
+        })
+    }
     render() {
         return (<div>
                 <Grid container spacing={24}>
                     <Grid item xs={6}>
-                        <OpportunityList opportunities={this.state.opportunities}/>
+                        <OpportunityList opportunities={this.state.opportunities} select={this._selectDetail}/>
                         <Button variant="fab" color="primary" aria-label="add" onClick={this._newOpp}>
                             <AddIcon/>
                         </Button>
@@ -146,8 +189,14 @@ class Opportunities extends Component {
                         <OpportunityDetail detail={this.state.detail} handleChange={this._handleChange}
                                            editing={this.state.editing}/>
                         <Button variant="fab" color="default" aria-label="add" onClick={this._saveDetail}>
-                            <SaveIcon/>
+                            {this.state.editing ? <SaveIcon/> : <EditIcon/>}
                         </Button>
+
+                        {this.state.editing && (
+                            <Button color="secondary" onClick={this._cancelEdit}>
+                               cancel
+                            </Button>
+                        )}
 
                     </Grid>
                 </Grid>
