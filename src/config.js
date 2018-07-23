a645074481fea0dc6149fc69a5adbd2deea8d9dd
nemesisesq <nemesisesq@gmail.com>
2018-07-22 22:21:11 -0400

    change host name

--- src/config.js
+++ src/config.js
@@ -2,7 +2,7 @@ const hostUri = () => {
     if (window.location.host.match(/localhost/)) {
         return "http://localhost:3000/api";
     } else {
-        return "https://groomly.herokuapp.com/api";
+        return "https://groomly-server.herokuapp.com/api";
     }
 
 }
