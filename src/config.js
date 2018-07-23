78779db3df9a425c55a4bfc1901dc399075b77fc
nemesisesq <nemesisesq@gmail.com>
2018-07-22 21:15:49 -0400

    using window

--- src/config.js
+++ src/config.js
@@ -1,6 +1,5 @@
 const hostUri = () => {
-//eslint-disable-next-line
-    if (location.host.match(/localhost/)) {
+    if (window.location.host.match(/localhost/)) {
         return "http://localhost:3000/api";
     } else {
         return "https://groomly.herokuapp.com/api";
